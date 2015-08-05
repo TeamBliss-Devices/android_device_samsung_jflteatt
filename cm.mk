a237259a0e76d03f90331a21f5ddd043a524a501
electrikjesus <electrikjesus@gmail.com>
2015-08-05 08:23:27 -0400

    Blissified the makefile

--- cm.mk
+++ cm.mk
@@ -1,10 +1,10 @@
 $(call inherit-product, device/samsung/jflteatt/full_jflteatt.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/bliss/config/nfc_enhanced.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common Bliss stuff.
+$(call inherit-product, vendor/bliss/config/common_full_phone.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
   PRODUCT_NAME=jflteuc \
@@ -12,5 +12,5 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
   BUILD_FINGERPRINT="samsung/jflteuc/jflteatt:5.0.1/LRX22C/I337UCUGOC3:user/release-keys" \
   PRIVATE_BUILD_DESC="jflteuc-user 5.0.1 LRX22C I337UCUGOC3 release-keys"
 
-PRODUCT_NAME := cm_jflteatt
+PRODUCT_NAME := bliss_jflteatt
 PRODUCT_DEVICE := jflteatt
